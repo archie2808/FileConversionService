827b967f3c10351e2a063452b8836fd235a0cc42
archie2808 <archiegledhill2808@gmail.com>
2024-04-09 13:42:07 +0100

    all converter subclasses moved to converter_logic directory in project root and initialised as a module with __init__.py. for the purpose of organisation in the codebase

--- converter_factory.py
+++ converter_factory.py
@@ -1,21 +1,20 @@
 from io import BytesIO
 from base_converter import BaseConverter
-from pdf_to_docx_converter import PDFToDocxConverter
-from docx_to_pdf_converter import DocxToPDFConverter
-from pdf_to_txt_converter import PDFToTXTConverter
-from docx_to_txt_converter import DOCXtoTXTConverter
-from txt_to_pdf_converter import TXTtoPDFConverter
-from txt_to_docx_converter import TXTToDocxConverter
-from txt_to_rtf_converter import TXTtoRTFConverter
-from rtf_to_docx_converter import RTFToDocxConverter
-from rtf_to_txt_converter import RTFToTXTConverter
-from rtf_to_pdf_converter import RTFtoPDFConverter
-from docx_to_rtf_converter import DocxToRTFConverter
-from pdf_to_rtf_converter import PDFToRTFConverter
-from csv_to_xlsx_converter import CSVtoXLSXConverter
-from xlsx_to_csv_converter import XlSXtoCSVConverter
-from image_converter import DynamicImageConverter
-
+from converter_logic.pdf_to_docx_converter import PDFToDocxConverter
+from converter_logic.docx_to_pdf_converter import DocxToPDFConverter
+from converter_logic.pdf_to_txt_converter import PDFToTXTConverter
+from converter_logic.docx_to_txt_converter import DOCXtoTXTConverter
+from converter_logic.txt_to_pdf_converter import TXTtoPDFConverter
+from converter_logic.txt_to_docx_converter import TXTToDocxConverter
+from converter_logic.txt_to_rtf_converter import TXTtoRTFConverter
+from converter_logic.rtf_to_docx_converter import RTFToDocxConverter
+from converter_logic.rtf_to_txt_converter import RTFToTXTConverter
+from converter_logic.rtf_to_pdf_converter import RTFtoPDFConverter
+from converter_logic.docx_to_rtf_converter import DocxToRTFConverter
+from converter_logic.pdf_to_rtf_converter import PDFToRTFConverter
+from converter_logic.csv_to_xlsx_converter import CSVtoXLSXConverter
+from converter_logic.xlsx_to_csv_converter import XlSXtoCSVConverter
+from converter_logic.image_converter import DynamicImageConverter
 
 
 class ConverterFactory:
