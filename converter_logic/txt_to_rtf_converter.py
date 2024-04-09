6b93e94da172d35f0239e9eaac65c3f0952785bb
archie2808 <archiegledhill2808@gmail.com>
2024-04-09 18:51:16 +0100

    thorough documentation added

--- converter_logic/txt_to_rtf_converter.py
+++ converter_logic/txt_to_rtf_converter.py
@@ -3,20 +3,7 @@ from base_converter import BaseConverter
 
 
 class TXTtoRTFConverter(BaseConverter):
-    """
-    Converts plain text to RTF format.
-
-    This converter generates RTF content from plain text input, applying basic RTF
-    formatting to the output.
-    """
-
     def convert(self, output_stream: BytesIO):
-        """
-        Creates RTF formatted text from the input plain text and writes it to the provided stream.
-
-        Parameters:
-            output_stream (BytesIO): The stream to write the RTF content to.
-        """
         # RTF header, document area start, and footer.
         rtf_header = r"{\rtf1\ansi\deff0{\fonttbl{\f0\fswiss Arial;}}\f0\pard "
         rtf_footer = r"\par}"
