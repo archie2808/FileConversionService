0b7cba3b821607b8147e3ff1e0c8b608fea12145
archie2808 <archiegledhill2808@gmail.com>
2024-04-20 21:20:53 +0100

    implemented load testing methods with locust

---
+++ locustfile.py
@@ -0,0 +1,27 @@
+from locust import HttpUser, task, between
+import os
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)  # Simulate wait time between requests of 1 to 5 seconds
+
+    @task(5)
+    def upload_and_convert(self):
+        with open("testfile.rtf", "rb") as file:
+            files = {
+                'file': file
+            }
+            data = {
+                'target_format': 'pdf'
+            }
+            self.client.post("/convert", files=files, data=data)
+
+    @task(5)
+    def convert_txt_to_pdf(self):
+        file_path = "testfile1.txt"  # Update this to the path of your sample text file
+        if os.path.exists(file_path):
+            with open(file_path, "rb") as file:
+                files = {'file': file}
+                data = {'target_format': 'pdf'}
+                self.client.post("/convert", files=files, data=data)
+        else:
+            print(f"File not found: {file_path}")
