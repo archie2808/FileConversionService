1de0a16ba70345e82a01d77b57a549eb7673e803
ag <ag@latest.myguest.virtualbox.org>
2024-04-25 11:31:29 +0100

    1600Load

--- src/config.py
+++ src/config.py
@@ -6,6 +6,6 @@ app = create_app()
 if __name__ == "__main__":
     app.run()
 
-#gunicorn -w 4 -b 0.0.0.0:5000 config:app
+#gunicorn -w 18 -b 0.0.0.0:5000 src.config:app
 #nmon -f -s 3 -c 200 -F outputfilename.nmon
 
